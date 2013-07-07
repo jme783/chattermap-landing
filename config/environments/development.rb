10cf2766c8ada921b5863dac6fb5fbab01bc1b97
Jeff Eiden <jeff.m.eiden@gmail.com>
2013-07-07 16:50:24 -0500

    configured email signups

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ RailsPrelaunchSignup::Application.configure do
   config.action_controller.perform_caching = false
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'chattermap.co' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = false
@@ -22,13 +22,10 @@ RailsPrelaunchSignup::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"]
   }
 
 
