143a10a4c3d1f2fca49995be7ef00099fb47da89
Jeff Eiden <jeff.m.eiden@gmail.com>
2013-07-07 17:10:25 -0500

    update user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130705225115) do
+ActiveRecord::Schema.define(:version => 20130707220740) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(:version => 20130705225115) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
