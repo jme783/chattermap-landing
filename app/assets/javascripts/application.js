97a9d9c733dc7172b12bdfd71d0268415ea483ed
Jeff Eiden <jeff.m.eiden@gmail.com>
2013-07-10 08:39:39 -0500

    improve design for 'Request Invitation' feature by adding AJAX to the modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,27 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+$('document').ready(function() {
+
+  // display validation errors for the "request invitation" form
+  if ($('.alert-error').length > 0) {
+    $("#request-invite").modal('toggle');
+  }
+
+  // use Ajax to submit the "request invitation" form
+  $('#invitation_button').on('click', function() {
+    var email = $('form #user_email').val();
+    var dataString = 'user[email]='+ email;
+    $.ajax({
+      type: "POST",
+      url: "/users",
+      data: dataString,
+      success: function(data) {
+        $('#request-invite').html(data);
+      }
+    });
+    return false;
+  });
+
+})
