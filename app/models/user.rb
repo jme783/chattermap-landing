c0fb0dfe3526e013a738d89c5b97f31cdf6db2b6
Jeff Eiden <jeff.m.eiden@gmail.com>
2013-07-10 21:49:56 -0500

    collect email addresses with MailChimp

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
+  after_create :add_user_to_mailchimp
+  before_destroy :remove_user_from_mailchimp
+  
   # override Devise method
   # no password is required when the account is created; validate password when the user sets one
   validates_confirmation_of :password
@@ -55,5 +58,35 @@ class User < ActiveRecord::Base
   def only_if_unconfirmed
     pending_any_confirmation {yield}
   end
+  
+  def add_user_to_mailchimp
+      return if email.include?(ENV['ADMIN_EMAIL'])
+      mailchimp = Gibbon.new
+      result = mailchimp.list_subscribe({
+        :id => ENV['MAILCHIMP_LIST_ID'],
+        :email_address => self.email,
+        :double_optin => false,
+        :update_existing => true,
+        :send_welcome => true
+        })
+      Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+    rescue Gibbon::MailChimpError => e
+      Rails.logger.info("MailChimp subscribe failed for #{self.email}: " + e.message)
+    end
+
+  def remove_user_from_mailchimp
+    mailchimp = Gibbon.new
+    result = mailchimp.list_unsubscribe({
+      :id => ENV['MAILCHIMP_LIST_ID'],
+      :email_address => self.email,
+      :delete_member => true,
+      :send_goodbye => false,
+      :send_notify => true
+      })
+    Rails.logger.info("Unsubscribed #{self.email} from MailChimp") if result
+  rescue Gibbon::MailChimpError => e
+    Rails.logger.info("MailChimp unsubscribe failed for #{self.email}: " + e.message)
+  end
+
   
 end
